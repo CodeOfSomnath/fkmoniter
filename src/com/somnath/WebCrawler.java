ef2cb03ee7d96f0c1e9c5bea9c4e7612eae55251
Somnath Nandi <somnathnandi368@gmail.com>
2024-11-22 00:49:48 +0530

    a basic structure

---
+++ src/com/somnath/WebCrawler.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2024 Somnath Nandi
+ * This class will be use to crawl all the data needed from internet
+ */
+
+package com.somnath;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class WebCrawler {
+    private String url;
+    private Document soup;
+
+    public WebCrawler(String url) {
+        this.url = url;
+    }
+
+
+    public void getPage() throws Exception {
+        Document soup = Jsoup.connect(this.url)
+            .userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+            .header("Sec-Fetch-Dest", "document")
+            .get();
+        this.soup = soup;
+    }
+
+    public void refresh() throws Exception {
+        // it is just look good
+        this.getPage();
+    }
+
+    public int getCurrentPrice() throws Exception {
+        this.refresh();
+        Elements ems =  soup.select(".hl05eU div");
+        return Integer.parseInt(ems.get(0).text().replaceAll("₹", "").replaceAll(",", ""));
+    }
+
+    public int getFullPrice() throws Exception {
+        this.refresh();
+        Elements ems =  soup.select(".hl05eU div");
+        return Integer.parseInt(ems.get(1).text().replaceAll("₹", "").replaceAll(",", ""));
+    }
+
+    public Document getDocument() {
+        return this.soup;
+    }
+
+}
